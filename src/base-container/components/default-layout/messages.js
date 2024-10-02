7b35ce7419dc0eeb3e8865552351785f6a080a1f
Syed Hamza <syed.hamza@data-pilot.com>
2024-10-02 11:02:41 +0500

    Update messages.js to Get Started With

--- src/base-container/components/default-layout/messages.js
+++ src/base-container/components/default-layout/messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'start.learning': {
     id: 'start.learning',
-    defaultMessage: 'Start learning',
+    defaultMessage: 'Get Started',
     description: 'Header text for logistration MFE pages',
   },
   'with.site.name': {
     id: 'with.site.name',
-    defaultMessage: 'with {siteName}',
+    defaultMessage: 'With {siteName}',
     description: 'Header text with site name for logistration MFE pages',
   },
 });
